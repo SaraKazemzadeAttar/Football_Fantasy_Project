ec669f157f457db64625758c350544cc798149ef
Reyhane Rahmani <ryhnrhmni22@gmail.com>
2023-06-15 10:08:50 +0430

    adding search in players list method on main branch in dataAccess layer

---
+++ SignUPAndLoginSection/DataAccessLayer/SearchInPlayersList.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+//using SignUPAndLoginSection.Model;
+using SignUPAndLoginSection.DataAccessLayer;
+using SignUPAndLoginSection.presentationLayer;
+using SignUPAndLoginSection.businessLayer;
+namespace SignUPAndLoginSection.DataAccessLayer;
+
+public class SearchInPlayersList
+{
+    public static List<string> Searchingmethod(string entry)
+    {
+        List<string> fullName = new List<string>();
+        List<string> foundNames = new List<string>();
+
+        foreach (var player in ListOfPlayers.getListOfPlayers())
+        {
+            fullName.Add(player.first_name + player.second_name);
+        }
+
+        foreach (var playerFullName in fullName)
+        {
+            if (playerFullName.Contains(entry))
+            {
+                foundNames.Add(playerFullName);
+            }
+        }
+
+        return foundNames;
+
+    }
+}
