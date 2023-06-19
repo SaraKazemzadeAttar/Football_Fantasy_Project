3ded7f553c6bd97f39b5778df0dea39ad06fd30e
reyhanehDavarpanah <r.davarpanah1382@gmail.com>
2023-06-19 22:52:23 +0330

    a little edit in teamPlayersSelection

--- SignUPAndLoginSection/DataAccessLayer/UsersTeamPlayers.cs
+++ SignUPAndLoginSection/DataAccessLayer/UsersTeamPlayers.cs
@@ -11,135 +11,140 @@ namespace SignUPAndLoginSection.DataAccessLayer;
 
 public class UsersTeamPlayers
 {
-     [Key]
-     public int UsersTeamPlayersId { get; set; }
-     
-     
-     [Display(Name = "userTable")]
-     public static int userId { get; set; }
+    [Key] public int UsersTeamPlayersId { get; set; }
 
-     [ForeignKey("userId")]
-     public User userTable { get; set; }
 
-     [Display(Name = "playerTable")]
-     public  int id{ get; set; }
+    [Display(Name = "userTable")] public static int userId { get; set; }
 
-     [ForeignKey("id")] 
-     public Player playerTable { get; set; }
-     
-     
-     //--------------------------------------------------------------- other fields
+    [ForeignKey("userId")] public User userTable { get; set; }
 
-     public bool isMainPLayer;
-     public static string selectionPlayerErrorMessage = "";
-     public static int counterOfGoalKeepers;
-     public static int counterOfDefenders;
-     public static int counterOfMidfielders;
-     public static int counterOfForwards;
+    [Display(Name = "playerTable")] public int id { get; set; }
 
-     public static void insertSelectedPlayerInUserTeam(Player player)
-     {
-          using (var db = new DataBase())
-          {
-               db.playerTable.Add(player);
-               db.SaveChanges();
-          }
-     }
-     
-     
-     public static int numberOfPlayersFromThisTeam(Player selectedPlayer)
-     {
-          var counterPlayersOfIntendedTeam = 0;
-          var intendedTeam = selectedPlayer.team;
-          using (var db = new DataBase())
-          {
-               foreach ( var player in db.UsersTeamPlayersTable)
-               {
-                    if (selectedPlayer.team == intendedTeam) 
-                         counterPlayersOfIntendedTeam++;
-               }
-          }
+    [ForeignKey("id")] public Player playerTable { get; set; }
 
-          return counterPlayersOfIntendedTeam;
-     }
 
-     public static bool hasTeamUnderTwoGoalKeepers()
-     {
-          using (var db = new DataBase())
-          {
-               foreach ( var player in db.UsersTeamPlayersTable)
-               {
-                    var post=FootballPlayersData.findPLayerByTheirId(player.id).element_type;
-                    if (Convert.ToInt16(post) == 0)
-                         counterOfGoalKeepers++;
-               }
-          }
+    //--------------------------------------------------------------- other fields
 
-          if (counterOfGoalKeepers < 2)
-               return true;
-          else
-          {
-               return false;
-          }
-     }
+    public bool isMainPLayer;
+    public static string selectionPlayerErrorMessage = "";
+    public static int counterOfGoalKeepers;
+    public static int counterOfDefenders;
+    public static int counterOfMidfielders;
+    public static int counterOfForwards;
 
-     public static bool hasTeamUnderFiveDefenders()
-     {
-          using (var db = new DataBase())
-          {
-               foreach ( var player in db.UsersTeamPlayersTable)
-               {
-                    var post=FootballPlayersData.findPLayerByTheirId(player.id).element_type;
-                    if (Convert.ToInt16(post) == 1)
-                         counterOfDefenders++;
-               }
-          }
+    public static void insertSelectedPlayerInUserTeam(Player player)
+    {
+        using (var db = new DataBase())
+        {
+            db.playerTable.Add(player);
+            db.SaveChanges();
+        }
+    }
 
-          if (counterOfDefenders< 5)
-               return true;
-          else
-          {
-               return false;
-          }
-     }
-     
-     public static bool hasTeamUnderFiveMidfielders()
-     {
-          using (var db = new DataBase())
-          {
-               foreach ( var player in db.UsersTeamPlayersTable)
-               {
-                    var post=FootballPlayersData.findPLayerByTheirId(player.id).element_type;
-                    if (Convert.ToInt16(post) == 2)
-                         counterOfMidfielders++;
-               }
-          }
 
-          if (counterOfMidfielders< 5)
-               return true;
-          else
-          {
-               return false;
-          }
-     }
-     
-     public static bool hasTeamUnderThreeForwards()
-     {
-          using (var db = new DataBase())
-          {
-               foreach ( var player in db.UsersTeamPlayersTable)
-               {
-                    var post=FootballPlayersData.findPLayerByTheirId(player.id).element_type;
-                    if (Convert.ToInt16(post) == 3)
-                         counterOfForwards++;
-               }
-          }
+    public static int numberOfPlayersFromThisTeam(Player selectedPlayer)
+    {
+        var counterPlayersOfIntendedTeam = 0;
+        var intendedTeam = selectedPlayer.team;
+        using (var db = new DataBase())
+        {
+            foreach (var player in db.UsersTeamPlayersTable)
+            {
+                if (selectedPlayer.team == intendedTeam)
+                {
+                    counterPlayersOfIntendedTeam++;
+                }
+            }
+        }
 
-          if (counterOfForwards< 3)
-               return true;
-          else
-          {
-               return false;
-          }
-     }
+        return counterPlayersOfIntendedTeam;
+    }
+
+    public static bool hasTeamUnderTwoGoalKeepers()
+    {
+        using (var db = new DataBase())
+        {
+            foreach (var player in db.UsersTeamPlayersTable)
+            {
+                var post = FootballPlayersData.findPLayerByTheirId(player.id).element_type;
+                if (Convert.ToInt16(post) == 0)
+                {
+                    counterOfGoalKeepers++;
+                }
+            }
+        }
+
+        if (counterOfGoalKeepers < 2)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool hasTeamUnderFiveDefenders()
+    {
+        using (var db = new DataBase())
+        {
+            foreach (var player in db.UsersTeamPlayersTable)
+            {
+                var post = FootballPlayersData.findPLayerByTheirId(player.id).element_type;
+                if (Convert.ToInt16(post) == 1)
+                {
+                    counterOfDefenders++;
+                }
+            }
+        }
+
+        if (counterOfDefenders < 5)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool hasTeamUnderFiveMidfielders()
+    {
+        using (var db = new DataBase())
+        {
+            foreach (var player in db.UsersTeamPlayersTable)
+            {
+                var post = FootballPlayersData.findPLayerByTheirId(player.id).element_type;
+                if (Convert.ToInt16(post) == 2)
+                {
+                    counterOfMidfielders++;
+                }
+            }
+        }
+
+        if (counterOfMidfielders < 5)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool hasTeamUnderThreeForwards()
+    {
+        using (var db = new DataBase())
+        {
+            foreach (var player in db.UsersTeamPlayersTable)
+            {
+                var post = FootballPlayersData.findPLayerByTheirId(player.id).element_type;
+                if (Convert.ToInt16(post) == 3)
+                {
+                    counterOfForwards++;
+                }
+            }
+        }
+
+        if (counterOfForwards < 3)
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
